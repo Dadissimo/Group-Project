a6d83ff9b22ebad76c5dce80e281dfd790484311
Dadissimo <32901797+Dadissimo@users.noreply.github.com>
2017-10-25 16:05:30 +0200

    Add files via upload

---
+++ Sandragil/Sandragil/MainWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sandragil
+{
+    public partial class MainWindow : Form
+    {
+        public List<User> Users;
+        public User Active;
+
+        public MainWindow(List<User> users, User active_user)
+        {
+            InitializeComponent();
+            Users = users;
+            Active = active_user;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText(Active.Name);
+        }
+    }
+}
---
+++ Sandragil/Sandragil/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Sandragil
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            StreamReader stream = new StreamReader(@"C:\Users\Dragan\source\repos\Sandragil\Users.txt");
+            List<User> users = new List<User>();
+            string username;
+            string password;
+            int id = 1;
+
+            while((username = stream.ReadLine()) != null)
+            {
+                password = stream.ReadLine();                
+                users.Add(new User(id, username, password));
+                id++;
+            }
+            stream.Close();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginWindow(users));
+        }
+    }
+}
