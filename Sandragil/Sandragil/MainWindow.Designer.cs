93d1308cca7f7cf18908ad3542c536e27a16fe10
shaass <33196524+shaass@users.noreply.github.com>
2017-10-29 13:29:15 +0100

    buttonherpes
    This reverts commit cb5cb102fe8db71d0feac4426e0db9568223452f.

--- Sandragil/Sandragil/MainWindow.Designer.cs
+++ Sandragil/Sandragil/MainWindow.Designer.cs
@@ -29,45 +29,50 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.herpes = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 138);
+            this.button1.Location = new System.Drawing.Point(339, 329);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(200, 55);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // herpes
             // 
-            this.button2.Location = new System.Drawing.Point(284, 138);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.herpes.Location = new System.Drawing.Point(757, 329);
+            this.herpes.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.herpes.Name = "herpes";
+            this.herpes.Size = new System.Drawing.Size(200, 55);
+            this.herpes.TabIndex = 1;
+            this.herpes.Text = "start herpes";
+            this.herpes.UseVisualStyleBackColor = true;
+            this.herpes.Click += new System.EventHandler(this.Start_Herpes);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(474, 138);
+            this.button3.Location = new System.Drawing.Point(1264, 329);
+            this.button3.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(200, 55);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(676, 292);
+            this.ClientSize = new System.Drawing.Size(1803, 696);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.herpes);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "MainWindow";
             this.Text = "MainWindow";
             this.ResumeLayout(false);
@@ -77,7 +82,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button herpes;
         private System.Windows.Forms.Button button3;
     }
 }
