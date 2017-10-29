0155ce3773e07a5a734fd95bcf5899564af16116
shaass <33196524+shaass@users.noreply.github.com>
2017-10-29 15:37:09 +0100

    Revert "s"
    This reverts commit 7ec3ecff4836bad2259727e67923a7a86db15133.

--- Sandragil/Sandragil/WindowAddUser.Designer.cs
+++ Sandragil/Sandragil/WindowAddUser.Designer.cs
@@ -28,78 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.UsernameTextbox = new System.Windows.Forms.TextBox();
-            this.PasswordTextBox = new System.Windows.Forms.TextBox();
-            this.AddUserButton = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(69, 68);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(228, 32);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Enter Username:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(70, 141);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(222, 32);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Enter Password:";
-            // 
-            // UsernameTextbox
-            // 
-            this.UsernameTextbox.Location = new System.Drawing.Point(491, 61);
-            this.UsernameTextbox.Name = "UsernameTextbox";
-            this.UsernameTextbox.Size = new System.Drawing.Size(100, 38);
-            this.UsernameTextbox.TabIndex = 2;
-            // 
-            // PasswordTextBox
-            // 
-            this.PasswordTextBox.Location = new System.Drawing.Point(491, 141);
-            this.PasswordTextBox.Name = "PasswordTextBox";
-            this.PasswordTextBox.Size = new System.Drawing.Size(100, 38);
-            this.PasswordTextBox.TabIndex = 3;
-            // 
-            // AddUserButton
-            // 
-            this.AddUserButton.Location = new System.Drawing.Point(269, 290);
-            this.AddUserButton.Name = "AddUserButton";
-            this.AddUserButton.Size = new System.Drawing.Size(192, 63);
-            this.AddUserButton.TabIndex = 4;
-            this.AddUserButton.Text = "Add User";
-            this.AddUserButton.UseVisualStyleBackColor = true;
-            this.AddUserButton.Click += new System.EventHandler(this.AddUserButton_Click);
-            // 
-            // WindowAddUser
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(903, 633);
-            this.Controls.Add(this.AddUserButton);
-            this.Controls.Add(this.PasswordTextBox);
-            this.Controls.Add(this.UsernameTextbox);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "WindowAddUser";
             this.Text = "WindowAddUser";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox UsernameTextbox;
-        private System.Windows.Forms.TextBox PasswordTextBox;
-        private System.Windows.Forms.Button AddUserButton;
     }
 }
