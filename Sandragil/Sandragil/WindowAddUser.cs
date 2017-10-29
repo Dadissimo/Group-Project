7ec3ecff4836bad2259727e67923a7a86db15133
shaass <33196524+shaass@users.noreply.github.com>
2017-10-29 15:36:24 +0100

    s

--- Sandragil/Sandragil/WindowAddUser.cs
+++ Sandragil/Sandragil/WindowAddUser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,32 @@ namespace Sandragil
 {
     public partial class WindowAddUser : Form
     {
+        public List<User> Users { get; set; }
+
         public WindowAddUser()
         {
             InitializeComponent();
+        }
+        public WindowAddUser(List<User> users)
+        {
+            InitializeComponent();
+            Users = users;
+        }
+
+   
+
+        private void AddUserButton_Click(object sender, EventArgs e)
+        {
+            var username = UsernameTextbox.Text;
+            string dir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string file = dir + @"\Users.txt";
+
+            File.AppendAllText(file, username);
+
+            var password = PasswordTextBox.Text;
+
+
+            File.AppendAllText(file, password);
         }
     }
 }
